8edba8f8334d3680c57f7041456f2a1fae7df431
Minchao Fu <minchao.fu@blackboard.com>
2017-04-05 15:54:29 +0800

    #master new script 'bing2google'

---
+++ bing2google.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name        bing2google
+// @namespace   com.github.fuminchao.monkeyscripts
+// @include     https://www.bing.com/search*
+// @run-at      document-end
+// @grant       GM_addStyle
+// @require     http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js
+// @version     1
+// ==/UserScript==
+
+(function( document, window, $, GM_addStyle ){
+  'use strict';
+
+  let buttonId = '__btn_google';
+
+  // add button
+  $(`<div id="${buttonId}" class="__btn_google b_searchboxSubmit">Google</div>`).appendTo($('#sb_form'));
+
+  // button style
+  GM_addStyle(`
+    #sb_form .b_searchboxSubmit#__btn_google {
+      display: inline-block;
+      border-width: 0;
+      margin: 0;
+
+      position:absolute;
+      background: transparent url(https://www.google.com/favicon.ico) no-repeat center center;
+    }
+  `);
+
+  // button action
+  $(document).delegate(`#${buttonId}`, 'click', function(){
+    var searchQ = (/\bq=([^\&]+)\&/.exec( window.location ) || ['',''])[1];
+    window.location = `https://www.google.com/search?q=${searchQ}&ie=utf-8&oe=utf-8`;
+  });
+
+})( document, window, window.jQuery, window.GM_addStyle );
